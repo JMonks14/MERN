ccbb4588d733fa7847c178d89e0552270090ef62
James Monks <James.Monks@academytrainee.com>
2020-08-10 13:02:09 +0100

    for loop exercise

---
+++ for.js
@@ -0,0 +1,11 @@
+let a=1
+let b=5
+let c=10
+
+let array = [5,1,98,3,8,1,96,7,2]
+
+for(; a<=10; ) {
+    console.log(a,`...`,b,`,,,`,c);
+    a++, b+=5, c+=10
+}
+// for (x in array) console.log(array[x]);
